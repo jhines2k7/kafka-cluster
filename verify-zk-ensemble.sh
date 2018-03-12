f9016030cde35dbe835cc1ee889f32fd0f99e1e9
James Hines <jameshines10@gmail.com>
2018-03-12 09:04:53 -0400

    Updates script to make it easier to verify ensemble

--- verify-zk-ensemble.sh
+++ verify-zk-ensemble.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-eval "$(docker-machine env zk-node-1)"
+node_name=zk-node-1
+
+if [ "$ENV" == "dev" ] ; then
+    node_name=kafka-node-1
+fi
+
+eval "$(docker-machine env $node_name)"
 
 docker run --net=kafka-net --rm confluentinc/cp-zookeeper:4.0.0 bash -c "echo stat | nc zk1 22181 | grep Mode"
 docker run --net=kafka-net --rm confluentinc/cp-zookeeper:4.0.0 bash -c "echo stat | nc zk2 32181 | grep Mode"
