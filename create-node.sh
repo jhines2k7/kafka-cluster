08bb3e4647354e86f6b291fdb2c417e733f4ed57
James Hines <jameshines10@gmail.com>
2018-03-12 06:52:39 -0400

    Adds logic to work with aws as a provider

--- create-node.sh
+++ create-node.sh
@@ -3,31 +3,38 @@
 node_type=$1
 node_name=$node_type
 
-# ami="ami-4f80b52a"
-# ami="ami-6a5f6a0f"
+if [ $PROVDER == "aws" ] ; then
+    instance_type=t2.large
+    ami="ami-4f80b52a"
+    # ami="ami-6a5f6a0f"
 
-# docker-machine create \
-# --engine-label "node.type=$1" \
-# --driver amazonec2 \
-# --amazonec2-ami $ami \
-# --amazonec2-vpc-id vpc-663eb80e \
-# --amazonec2-subnet-id subnet-757e4b38 \
-# --amazonec2-security-group kafka-cluster-dev \
-# --amazonec2-instance-type t2.large \
-# --amazonec2-region us-east-2 \
-# --amazonec2-zone c \
-# $1
+    if [ "$node_type" == "webtools" ] ; then
+        instance_type=t2.small
+    fi
 
-size=4gb
+    docker-machine create \
+    --engine-label "node.type=$node_type" \
+    --driver amazonec2 \
+    --amazonec2-ami $ami \
+    --amazonec2-vpc-id vpc-663eb80e \
+    --amazonec2-subnet-id subnet-757e4b38 \
+    --amazonec2-security-group kafka-cluster-dev \
+    --amazonec2-instance-type t2.large \
+    --amazonec2-region us-east-2 \
+    --amazonec2-zone c \
+    $node_name
+else
+    size=4gb
 
-if [ "$node_type" == "webtools" ] ; then
-    size=2gb
+    if [ "$node_type" == "webtools" ] ; then
+        size=2gb
+    fi
+
+    docker-machine create \
+    --engine-label "node.type=$node_type" \
+    --driver digitalocean \
+    --digitalocean-image ubuntu-17-10-x64 \
+    --digitalocean-size $size \
+    --digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
+    $node_name
 fi
-
-docker-machine create \
---engine-label "node.type=$node_type" \
---driver digitalocean \
---digitalocean-image ubuntu-17-10-x64 \
---digitalocean-size $size \
---digitalocean-access-token $DIGITALOCEAN_ACCESS_TOKEN \
-$node_name
