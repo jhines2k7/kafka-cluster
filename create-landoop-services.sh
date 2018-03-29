ca13ecaa7bf8ec2590eb2f45f552bc090f304812
James Hines <jameshines10@gmail.com>
2018-03-29 16:43:33 -0400

    How am I fucking this up so badly?

--- create-landoop-services.sh
+++ create-landoop-services.sh
@@ -12,7 +12,7 @@ docker service create \
 --name kafkatopicsui \
 --network kafkanet \
 -p 8000:8000 \
--e "KAFKA_REST_PROXY_URL=http://kafkarest:8082" \
+-e KAFKA_REST_PROXY_URL="http://kafkarest:8082" \
 -e PROXY=true \
 --constraint="engine.labels.node.type==landoop" \
 landoop/kafka-topics-ui &
@@ -21,7 +21,7 @@ docker service create \
 --name schemaregistryui \
 --network kafkanet \
 -p 8100:8000 \
--e "SCHEMAREGISTRY_URL=http://schemaregistry:8081" \
+-e SCHEMAREGISTRY_URL="http://schemaregistry:8081" \
 --constraint="engine.labels.node.type==landoop" \
 landoop/schema-registry-ui &
 
