e7d2cfabfcc772289fb8caf3e11400344be9170d
James Hines <jameshines10@gmail.com>
2018-03-29 14:25:53 -0400

    Fixes syntax errors

--- create-kafka-services.sh
+++ create-kafka-services.sh
@@ -21,25 +21,25 @@ eval "$(docker-machine env $node_name)"
 docker service create \
 --name kafka1 \
 --network kafkanet \
--e KAFKA_ZOOKEEPER_CONNECT zk1:22181,zk2:32181,zk3:42181 \
--e KAFKA_ADVERTISED_LISTENERS PLAINTEXT://kafka1:29092 \
---constraint "engine.labels.node.type==$node_type_1" \
+-e KAFKA_ZOOKEEPER_CONNECT="zk1:22181,zk2:32181,zk3:42181" \
+-e KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka1:29092" \
+--constraint="engine.labels.node.type==$node_type_1" \
 confluentinc/cp-kafka:4.0.0 &
 
 docker service create \
 --name kafka2 \
 --network kafkanet \
--e KAFKA_ZOOKEEPER_CONNECT zk1:22181,zk2:32181,zk3:42181 \
--e KAFKA_ADVERTISED_LISTENERS PLAINTEXT://kafka2:39092 \
---constraint "engine.labels.node.type==$node_type_2" \
+-e KAFKA_ZOOKEEPER_CONNECT="zk1:22181,zk2:32181,zk3:42181" \
+-e KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka2:39092" \
+--constraint="engine.labels.node.type==$node_type_2" \
 confluentinc/cp-kafka:4.0.0 &
 
 docker service create \
 --name kafka3 \
 --network kafkanet \
--e KAFKA_ZOOKEEPER_CONNECT zk1:22181,zk2:32181,zk3:42181 \
--e KAFKA_ADVERTISED_LISTENERS PLAINTEXT://kafka3:49092 \
---constraint "engine.labels.node.type==$node_type_3" \
+-e KAFKA_ZOOKEEPER_CONNECT="zk1:22181,zk2:32181,zk3:42181" \
+-e KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka3:49092" \
+--constraint="engine.labels.node.type==$node_type_3" \
 confluentinc/cp-kafka:4.0.0 &
 
 wait
